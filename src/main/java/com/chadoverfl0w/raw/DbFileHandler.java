2d6b86bef2e48c73621dbcd18dfef2761dfbead0
jp <johnamata@protonmail.com>
2022-10-26 02:33:52 +0800

    implement db file handler

--- src/main/java/com/chadoverfl0w/raw/DbFileHandler.java
+++ src/main/java/com/chadoverfl0w/raw/DbFileHandler.java
@@ -1,4 +1,15 @@
 package com.chadoverfl0w.raw;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+
 public class DbFileHandler {
+    private RandomAccessFile dbFile;
+    public DbFileHandler(final String dbFileName) throws FileNotFoundException {
+        this.dbFile = new RandomAccessFile(dbFileName, "rw");
+    }
+    public void closeDbFile() throws IOException {
+        this.dbFile.close();
+    }
 }
