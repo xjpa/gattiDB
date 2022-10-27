ddedfdbe4f8d7f0574c2f6d208295452ab7b54d3
jp <johnamata@protonmail.com>
2022-10-27 17:29:20 +0800

    implement db record adding

--- src/main/java/com/chadoverfl0w/gattiDB/raw/DbFileHandler.java
+++ src/main/java/com/chadoverfl0w/gattiDB/raw/DbFileHandler.java
@@ -9,6 +9,44 @@ public class DbFileHandler {
     public DbFileHandler(final String dbFileName) throws FileNotFoundException {
         this.dbFile = new RandomAccessFile(dbFileName, "rw");
     }
+    //db for that buggati
+    //todo: may refactor later to be more generic?
+    public boolean addData(String name,
+                           int age,
+                           String address,
+                           String plateNumber,
+                           String description) throws IOException {
+        this.dbFile.seek(this.dbFile.length());
+        int recordLength =  4 +
+                name.length() +
+                4 +
+                4 +
+                address.length() +
+                4 +
+                plateNumber.length() +
+                4 +
+                description.length();
+
+        this.dbFile.writeBoolean(false);
+
+        this.dbFile.writeInt(recordLength);
+
+        this.dbFile.writeInt(name.length());
+        this.dbFile.write(name.getBytes());
+
+        this.dbFile.writeInt(age);
+
+        this.dbFile.writeInt(address.length());
+        this.dbFile.write(address.getBytes());
+
+        this.dbFile.writeInt(plateNumber.length());
+        this.dbFile.write(plateNumber.getBytes());
+
+        this.dbFile.writeInt(description.length());
+        this.dbFile.write(description.getBytes());
+
+        return true;
+    }
     public void closeDbFile() throws IOException {
         this.dbFile.close();
     }
